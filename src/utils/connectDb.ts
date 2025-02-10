4626611e2007046a615b00547969b97654be84a0
zeddted360 <106580586+zeddted360@users.noreply.github.com>
2025-02-10 16:09:46 +0100

    finished and updated my env file

--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-  const uri = process.env.MONGO_LOCAL;
+  const uri = process.env.MONGO_URL;
   if (!uri) throw new Error("MONGO_URI is not defined");
   const ready_state: number = mongoose.connection.readyState;
   if (ready_state === 1) return console.log("DB already Connected");
